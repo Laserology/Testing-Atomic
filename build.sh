b824d837a8e2eccfef3adcf5a6a8b60eab5a9e54
Laserology <167719528+Laserology@users.noreply.github.com>
2024-07-15 10:50:04 -0700

    Upgrade to F40

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ RELEASE="$(rpm -E %fedora)"
 # Packages can be installed from any enabled yum repo on the image.
 # RPMfusion repos are available by default in ublue main images
 # List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
+# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/40/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
 rpm-ostree install screen
